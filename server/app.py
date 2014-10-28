4e55e0e05d2c69f585394f286fab9888d04ff6dd
jwqchen <jwqchen@gmail.com>
2014-10-27 23:15:55 -0700

    javascript, flask edits
    John - I added a couple more scenarios for flask and wrote them in
    psuedo code.
    
    Everyone - In home.html, I drafted a javascript for validating the long
    URL. I'm having trouble running the program on my local computer so I
    can't check if the JS works. Can someone take a look?

--- server/app.py
+++ server/app.py
@@ -22,13 +22,43 @@ def randomize():
 def home():
     return flask.render_template('home.html')
 
+# def check_1(short, long_url):
+#     if short in db and db[short] != long_url:
+#         return flask.render_template('home.html', taken_url="Short url taken. Please try again.")
+#     if db[short] = long_url:
+#         return flask.render_template('home.html', new_url = short) 
+
+# def check_2():
+#     long_url = request.form['long-url'].encode('utf-8')
+#     short = request.form['short-url'].encode('utf-8')
+#     check_1(short, long_url)
+#     if db.key(long_url) != None and db.key(long_url) != short:
+#         flask.render_template('home.html', taken_url="The URL already has been shortened before with the value", short, ", do you want to keep it or create a new one?") 
+#         answer = raw_input("y/n"+"\n")
+#         if answer = "y":
+#             flask.render_template('home.html')
+
+
+
 
 @app.route('/shorts', methods=['POST'])
 def shorts():
     long_url = request.form['long-url'].encode('utf-8')
     short = request.form['short-url'].encode('utf-8')
-    if short in db:
+
+    if short in db and db[short] != long_url:
         return flask.render_template('home.html', taken_url="Short url taken. Please try again.")
+    elif db[short] = long_url:
+        return flask.render_template('home.html', new_url = short) 
+    # my syntax is probably wrong...
+    elif db.key(long_url) != None and db.key(long_url) != short:
+        flask.render_template('home.html', taken_url="The URL already has been shortened before with the value", short, ", do you want to keep it or create a new one?") 
+        answer = raw_input("y/n"+"\n")
+        if answer = "y":
+            make the new url
+        if answer = "n":
+            tell user the old url
+        
     else:
         if short:
             db[short] = long_url
