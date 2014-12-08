d568334aa1f7ce339f7050b957b47c21a5f37be9
semerdj <jsemer@gmail.com>
2014-12-08 11:44:27 -0800

    pep8

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from flask import render_template, flash, redirect, session, url_for, request, g, abort, jsonify
-from flask.ext.login import login_user, logout_user, current_user, login_required
+from flask import render_template, flash, redirect, \
+    session, url_for, request, g, abort, jsonify
+from flask.ext.login import login_user, logout_user, \
+    current_user, login_required
 from app import app, db, login_manager
 from forms import LoginForm, RegistrationForm, ShortenForm
 from models import User, Link, Click, bcrypt
@@ -27,7 +29,8 @@ def register():
 
         if form_is_valid:
             username = request.form['username']
-            username_exist = User.query.filter_by(username=username).first()
+            username_exist = User.query.\
+                filter_by(username=username).first()
 
             if username_exist is not None:
                 flash("Username already in use. Please try again.")
@@ -54,7 +57,7 @@ def register():
 def login():
     form = LoginForm(request.form)
 
-    if request.method == 'GET': 
+    if request.method == 'GET':
         return render_template("login.html", form=form)
 
     elif request.method == 'POST':
@@ -63,23 +66,24 @@ def login():
         if form_is_valid:
             username = form.username.data
             password = form.password.data
-            registered_user = User.query.filter_by(username=username).first()
+            registered_user = User.query.\
+                filter_by(username=username).first()
 
             remember_me = False
             if 'remember_me' in request.form:
                 remember_me = True
 
-            if registered_user is not None and bcrypt.check_password_hash(
-                registered_user.password, password
-                ):
-                login_user(registered_user, remember=remember_me)    
+            if registered_user is not None and \
+                bcrypt.check_password_hash(
+                    registered_user.password, password
+                    ):
+                login_user(registered_user, remember=remember_me)
                 flash('Logged in successfully')
                 return redirect(request.args.get('next') or url_for('index'))
 
             else:
-                flash('Username or password is invalid' , 'error')
+                flash('Username or password is invalid', 'error')
                 return render_template('login.html', form=form)
-                #return redirect(url_for('login'))
 
         else:
             return render_template('login.html', form=form)
@@ -88,6 +92,7 @@ def login():
 @app.route('/logout')
 def logout():
     logout_user()
+    flash('You were logged out.')
     return redirect(url_for('index'))
 
 
@@ -105,10 +110,13 @@ def shorts():
         longurl = form.longurl.data
         shorturl = form.shorturl.data
         form_is_valid = form.validate_on_submit()
-        user_is_logged_in = (g.user is not None and g.user.is_authenticated())
+        user_is_logged_in = (
+            g.user is not None and g.user.is_authenticated()
+            )
 
         if form_is_valid:
-            shorturl_query = Link.query.filter_by(shorturl=shorturl).first()
+            shorturl_query = Link.query.\
+                filter_by(shorturl=shorturl).first()
 
             if shorturl_query is not None:
                 flash('Link already exists. Choose again.')
@@ -116,16 +124,16 @@ def shorts():
 
             else:
                 link = Link(longurl, shorturl)
-                
+
                 if user_is_logged_in:
                     link.user = g.user
 
                 db.session.add(link)
                 db.session.commit()
                 flash('Link successfully registered')
-                return render_template('shorts.html', 
-                    shorturl='{0}s/{1}'.\
-                    format(request.url_root, link.shorturl))
+                return render_template(
+                    'shorts.html',
+                    shorturl='{}s/{}'.format(request.url_root, link.shorturl))
 
         elif not form_is_valid:
             return render_template('index.html', form=form)
@@ -136,12 +144,11 @@ def shorts():
 
 @app.route('/s/<url>', methods=['GET'])
 def shorts_redirect(url):
-
     if request.method == 'GET':
         url_query = Link.query.filter_by(shorturl=url).first()
 
         if url_query is None:
-            flash('Link not found') #return abort(404)
+            flash('Link not found')  # return abort(404)
             return redirect(url_for('index'))
 
         else:
@@ -162,17 +169,22 @@ def profile(username):
     user = User.query.filter_by(id=g.user.id).first()
 
     if user is None:
-        flash('User %s not found.' % username)
+        flash('User {} not found.'.format(username))
         return redirect(url_for('index'))
 
     links = Link.query.join(User, (User.id == Link.user_id)).\
         filter(User.username == user.username).\
         order_by(Link.timestamp.desc())
 
-    updated_links = [(request.url_root + 's/' + link.shorturl, link.longurl) for link in links]
+    print db.session.query(
+        Click.shorturl, db.func.count(
+            Click.shorturl)).group_by(Click.shorturl).all()
+
+    append_url = lambda x: (request.url_root + 's/' + x.shorturl, x.longurl)
+    updated_links = [append_url(x) for x in links]
 
     return render_template("user.html",
                            title='Home',
                            user=user,
                            links=updated_links)
-    #return jsonify(links=[link.serialize() for link in links])
+    # return jsonify(links=[link.serialize() for link in links])
