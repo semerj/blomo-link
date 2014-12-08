67f1f6fda925ba31816bac1317a543ec1789aeeb
semerdj <jsemer@gmail.com>
2014-12-07 18:50:39 -0800

    add email and regex validators

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,14 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, URL
+from wtforms.validators import DataRequired, Length, Email, URL, Regexp
 
 
 class LoginForm(Form):
     username = StringField('Username',
         validators=[DataRequired(message=u'This field is required')])
     password = StringField('Password',
-        validators=[DataRequired(message=u'This field is required')])
+        validators=[DataRequired(message=u'This field is required'),
+                    Email(message=u'Please enter a valid email address')])
     remember_me = BooleanField('remember_me', default=False)
 
 
@@ -16,31 +17,19 @@ class ShortenForm(Form):
         validators=[DataRequired(message=u'This field is required'), 
                     URL(message=u'Please provide a properly formatted URL')])
     shorturl = StringField('Short name',
-        validators=[DataRequired(message=u'This field is required')])
-
+        validators=[DataRequired(message=u'This field is required'),
+                    Regexp(regex=r'[A-Za-z0-9]', 
+                           message=u'Please use only alphanumeric characters')])
 
 class RegistrationForm(Form):
     username = StringField('New username',
         validators=[DataRequired(message=u'This field is required'), 
-                    Length(min=4, max=20, message=u'Please enter password at least 4 characters long')])
+                    Length(min=4, max=20, message=u'Please enter at least 4 characters'),
+                    Regexp(regex=r'[A-Za-z0-9]',
+                           message=u'Please use only alphanumeric characters')])
     password = StringField('New password',
         validators=[DataRequired(message=u'This field is required'), 
-                    Length(min=4, message=u'Please enter password at least 4 characters long')])
+                    Length(min=4, message=u'Please enter at least 4 characters')])
     email = StringField('Email',
-        validators=[DataRequired(message=u'This field is required')])
-
-'''
-    def __init__(self, *args, **kwargs):
-        Form.__init__(self, *args, **kwargs)
-
-    def validate(self):
-        if not Form.validate(self):
-            return False
-
-        user = User.query.filter_by(email = self.email.data.lower()).first()
-        if user:
-            self.email.errors.append("That email is already taken")
-            return False
-        else:
-            return True
-'''
+        validators=[DataRequired(message=u'This field is required'),
+                    Email(message=u'Please enter a valid email address')])
