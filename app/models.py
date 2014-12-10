774e88434cff30a9f71bb33e9162a2f96fdc7b91
semerdj <jsemer@gmail.com>
2014-12-09 22:48:43 -0800

    going back 2

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(255), nullable=False, default='')
     email = db.Column(db.String(120), index=True, unique=False)
-    timestamp = db.Column(db.DateTime, default=dgit statetime.datetime.utcnow)
+    timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     links = db.relationship('Link', backref='user', lazy='dynamic')
 
     def __init__(self, username, password, email):
