280ec631d994b8a6ed43cd380728bfb929f90830
semerdj <jsemer@gmail.com>
2014-12-10 22:58:13 -0800

    remove files

--- run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from app import app
-import logging
-
-#logger = logging.getLogger('werkzeug')
-#handler = logging.FileHandler('access.log')
-#logger.addHandler(handler)
-
-app.run(debug=True)
