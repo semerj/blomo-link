c2c7b66393df88fd24b9bbe5072ccaef225595d4
semerdj <jsemer@gmail.com>
2014-12-11 23:05:08 -0800

    heroku vs local config

--- config.py
+++ config.py
@@ -3,5 +3,8 @@ import os
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'jimblomo'
 
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+# for heroku w/ postgres
+# SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
+# for local w/ mysql
+SQLALCHEMY_DATABASE_URI = 'mysql://apps:password@localhost/apps'
